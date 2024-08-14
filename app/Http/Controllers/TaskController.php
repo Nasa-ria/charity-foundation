aa93934ce1345eb57d986ebb1834bf1c59a3a790
Nasaria <mumuninasaria@gmail.com>
2024-08-14 10:43:43 +0000

    trying to push code

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,7 +12,7 @@ class TaskController extends Controller
 {
     public function index()
     {
-        // $tasks = Task::all(); 
+        $tasks = Task::all(); 
         return view('dashboard.admin.pages.tasks.task');;
     }
 
